b8f0b30850064fe33202fa9539b576e5224a2c6e
anarchih <anarchih.tw@gmail.com>
2015-05-12 23:56:56 +0800

    implement solver

--- Puzzle.py
+++ Puzzle.py
@@ -1,38 +1,39 @@
-from queue import PriorityQueue
+from queue import PriorityQueue, Queue
 from copy import deepcopy
+import time
 GOAL_4X4 = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
 GOAL_3X3 = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 
 class Puzzle():
-    def __init__(self, width, height, board):
+    def __init__(self, width, height, board, pointer):
         self.width = width
         self.height = height
         self.board = board
         self.str_ = str()
-        self.pointer = self.findPointer()
+        self.pointer = pointer
 
-    def solved(self):
-        return self.board == GOAL_3X3
+    def solved(self, goal):
+        return self.board == goal
 
     def possible_moves(self):
         j, i = self.pointer
         if i > 0:
             b = deepcopy(self.board)
             b[i - 1][j], b[i][j] = self.board[i][j], self.board[i - 1][j]
-            yield Puzzle(self.width, self.height, b)
+            yield Puzzle(self.width, self.height, b, (j, i - 1))
         if i < self.width - 1:
             b = deepcopy(self.board)
             b[i + 1][j], b[i][j] = self.board[i][j], self.board[i + 1][j]
-            yield Puzzle(self.width, self.height, b)
+            yield Puzzle(self.width, self.height, b, (j, i + 1))
         if j > 0:
             b = deepcopy(self.board)
             b[i][j - 1], b[i][j] = self.board[i][j], self.board[i][j - 1]
-            yield Puzzle(self.width, self.height, b)
+            yield Puzzle(self.width, self.height, b, (j - 1, i))
         if j < self.height - 1:
             b = deepcopy(self.board)
             b[i][j + 1], b[i][j] = self.board[i][j], self.board[i][j + 1]
-            yield Puzzle(self.width, self.height, b)
+            yield Puzzle(self.width, self.height, b, (j + 1, i))
 
     def findPointer(self):
         p = (0, 0)
@@ -46,27 +47,38 @@ class Puzzle():
         return self.board == other.board
 
     def __hash__(self):
-        return hash(str(self.board))
+        return hash(str(self.board) + (str(self.pointer)))
 
 
 class Node():
-    def __init__(self, puzzle, g, n):
+    def __init__(self, puzzle, g, back, goal):
         self.puzzle = puzzle
         self.solved = False
-        self.h = self.H()
+        self.h = self.H(goal)
         self.g = g
         self.f = self.h + self.g
-        self.back = n
+        self.back = back
 
-    def H(self):
+    def H(self, goal):
+        tmp = [Queue(), Queue(), Queue(), Queue()]
+        total = 0
         p = self.puzzle
         t = 0
-        for i in range(p.width):
-            for j in range(p.height):
-                a = int(p.board[i][j] / p.width)
-                b = p.board[i][j] % p.width
-                t = abs(a - j) + abs(b - i)
-        return t * 10
+        for i in range(p.height):
+            for j in range(p.width):
+                tmp[p.board[i][j]].put(i * p.width + j)
+
+        for i in range(p.height):
+            for j in range(p.width):
+                a = tmp[goal[i][j]].get()
+                total += abs(a - j - i * p.width)
+
+        # for i in range(p.width):
+        #     for j in range(p.height):
+        #         a = int(p.board[i][j] / p.width)
+        #         b = p.board[i][j] % p.width
+        #         t = abs(a - j) + abs(b - i)
+        return total * 5
 
     def __lt__(self, other):
         return self.f < other.f
@@ -85,29 +97,43 @@ class Node():
 
 
 class Solver():
-    def __init__(self, start):
+    def __init__(self, start, goal):
         self.start = start
+        self.bestNode = None
+        self.goal = goal
 
     def solve(self):
         q = PriorityQueue()
-        q.put(Node(self.start, 0, None))
+        self.bestNode = Node(self.start, 0, None, self.goal)
+        q.put(self.bestNode)
         s = set()
         s.add(self.start)
-        if self.start.solved():
+        if self.start.solved(self.goal):
             print(self.start.board)
             return 1
         while not q.empty():
             node = q.get()
             for p in node.puzzle.possible_moves():
                 if p not in s:
-                    n = Node(p, node.g + 1, node)
+                    n = Node(p, node.g + 1, node, self.goal)
                     s.add(n.puzzle)
                     q.put(n)
-                    if n.puzzle.solved():
+                    if n.puzzle.solved(self.goal):
                         return n
 
 
+def printPuzzle(p):
+    b = p.board
+    for i in range(p.height):
+        for j in range(p.width):
+            print(b[i][j], end=' ')
+            pass
+        print("")
+    print("")
+
+
 def main():
+    then = time.time()
     b = [[9, 6, 2, 13],
          [8, 15, 7, 11],
          [12, 3, 4, 1],
@@ -115,15 +141,26 @@ def main():
     b = [[5, 1, 6],
          [0, 3, 8],
          [4, 2, 7]]
-    p = Puzzle(3, 3, b)
-    s = Solver(p)
+    b = [[0, 2, 1], [0, 1, 2], [0, 1, 2]]
+    b = [[1, 2, 2],
+         [0, 2, 1],
+         [0, 1, 0]]
+    b = [[0, 1, 1, 0], [3, 3, 1, 2], [3, 0, 2, 2], [0, 3, 1, 2]]
+    g = [[0, 1, 2, 3],
+         [0, 1, 2, 3],
+         [0, 1, 2, 3],
+         [0, 1, 2, 3]]
+    p = Puzzle(4, 4, b, (3, 3))
+    s = Solver(p, g)
     count = 0
     q = s.solve()
     while q:
-        print(q.puzzle.board)
+        printPuzzle(q.puzzle)
         q = q.back
         count += 1
+    now = time.time()
     print(count - 1)
+    print(now - then)
 
 if __name__ == '__main__':
     main()
