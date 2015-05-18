a19a124742452b323d84251d800e5374570e458c
anarchih <anarchih.tw@gmail.com>
2015-05-18 17:55:37 +0800

    change some color code

--- Color.py
+++ Color.py
@@ -1,8 +1,8 @@
 class Color():
         red = (255, 0, 0)
-        green = (0, 255, 0)
+        green = (10, 160, 10)
         blue = (0, 0, 255)
-        yellow = (255, 255, 0)
+        yellow = (255, 200, 0)
         pink = (255, 0, 255)
         purple = (128, 0, 255)
         c = [red, green, blue, yellow, pink, purple]
