a2ab39cbbb600580919d83105f22f89d98596e8f
anarchih <anarchih.tw@gmail.com>
2015-05-25 14:42:59 +0800

    clean branch

--- Color.py
+++
@@ -1,8 +0,0 @@
-class Color():
-        red = (255, 0, 0)
-        green = (0, 255, 0)
-        blue = (0, 0, 255)
-        yellow = (255, 255, 0)
-        pink = (255, 0, 255)
-        purple = (128, 0, 255)
-        c = [red, green, blue, yellow, pink, purple]
